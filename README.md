02e538399ddc5ae3021ca9cc332efc8ed32dabc2
josephW1805 <joseph.weng.1805@gmail.com>
2025-05-26 11:43:27 -0400

    Part 1 Basic layout

---
+++ README.md
@@ -0,0 +1,25 @@
+## PART 1 Basic layout
+
+- Add logo asset ✅
+- Basic app router setup ✅
+- Sidebar component ✅
+  - Sidebar sections ✅
+  - Sidebar items ✅
+- Navbar ✅
+  - Search input ✅
+  - Sign in component ✅
+
+## PART 2 Authentication
+
+- Integrate clerk
+- Add Sign in screens
+- Add UserButton
+- Add middleware
+- Use auth state on sidebar sections
+- Protect routes
+
+## Deploy on Vercel
+
+The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
+
+Check out our [Next.js deployment documentation](https://nextjs.org/docs/app/building-your-application/deploying) for more details.
---
+++ src/modules/home/ui/components/home-sidebar/personal-section.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import {
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import { History, ListVideo, ThumbsUp } from "lucide-react";
+import Link from "next/link";
+
+const items = [
+  {
+    title: "History",
+    url: "/playlists/history",
+    icon: History,
+    auth: true,
+  },
+  {
+    title: "Liked videos",
+    url: "/playlists/liked",
+    icon: ThumbsUp,
+    auth: true,
+  },
+  {
+    title: "ALl playlists",
+    url: "/playlists",
+    icon: ListVideo,
+    auth: true,
+  },
+];
+
+export const PersonalSection = () => {
+  return (
+    <SidebarGroup>
+      <SidebarGroupLabel>You</SidebarGroupLabel>
+      <SidebarGroupContent>
+        <SidebarMenu>
+          {items.map((item) => (
+            <SidebarMenuItem key={item.title}>
+              <SidebarMenuButton
+                tooltip={item.title}
+                asChild
+                isActive={false} // TODO: Change to look at current pathname
+                onClick={() => {}} // TODO: Do something on click
+              >
+                <Link href={item.url} className="flex items-center gap-4">
+                  <item.icon />
+                  <span className="text-sm">{item.title}</span>
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          ))}
+        </SidebarMenu>
+      </SidebarGroupContent>
+    </SidebarGroup>
+  );
+};
